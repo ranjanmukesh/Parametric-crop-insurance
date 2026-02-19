733bac809190224787e40d7e286a93b87411b426
Mukesh Ranjan <mukesh.ranjan@tutamail.com>
2026-02-19 09:27:21 +0000

    fixed typos

--- src/ParametricCropInsurance.sol
+++ src/ParametricCropInsurance.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/functions/v1.0.0/FunctionsClient.sol";
-import "@chainlink/contracts/src/v0.8/functions/v1.0.0/FunctionsRequest.sol";
+import "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import "@chainlink/contracts/src/v0.8/libraries/v1_0_0/FunctionsRequest.sol";
 
 contract ParametricCropInsurance is FunctionsClient, ConfirmedOwner {
 	
