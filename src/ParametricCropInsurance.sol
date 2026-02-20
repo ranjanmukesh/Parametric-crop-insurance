51f8a3a83c6e5370f2daf04c30ba6a731af06df6
Mukesh Ranjan <mukesh.ranjan@tutamail.com>
2026-02-20 03:47:27 +0000

    copied imports from chainlink site

--- src/ParametricCropInsurance.sol
+++ src/ParametricCropInsurance.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
-import "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
 contract ParametricCropInsurance is FunctionsClient, ConfirmedOwner {
 	
