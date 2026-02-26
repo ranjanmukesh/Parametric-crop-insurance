e6b027a8160e6adc06c2d7e4c6f425bb94d849e8
Mukesh Ranjan <mukesh.ranjan@tutamail.com>
2026-02-26 07:02:57 +0000

    commented out the private key part in deployment script as it is directly passed in CI

--- script/DeployInsurance.s.sol
+++ script/DeployInsurance.s.sol
@@ -8,7 +8,7 @@ import {console} from "forge-std/console.sol";
 
 contract DeployInsurance is Script {
 	function run() external {
-		uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+//		uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
 		address router = 0xC22a79eBA640940ABB6dF0f7982cc119578E11De;
 		bytes32 donId = bytes32("fun-polygon-amoy-1");
