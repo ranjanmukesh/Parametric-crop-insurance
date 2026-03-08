c493125e421d372ce2277b39bba6a517fab97e5d
Mukesh Ranjan <mukesh.ranjan@tutamail.com>
2026-03-08 07:31:08 +0000

    commented cache dependency path

--- contracts/script/DeployInsurance.s.sol
+++ contracts/script/DeployInsurance.s.sol
@@ -10,8 +10,8 @@ contract DeployInsurance is Script {
 	function run() external {
 //		uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
-		address router = 0xC22a79eBA640940ABB6dF0f7982cc119578E11De;
-		bytes32 donId = bytes32("fun-polygon-amoy-1");
+		address router = 0xf9B8fc078197181C841c296C876945aaa425B278;
+		bytes32 donId = 0x66756e2d626173652d7365706f6c69612d310000000000000000000000000000;
 		uint64 subscriptionId = 123;
 
 		vm.startBroadcast();
