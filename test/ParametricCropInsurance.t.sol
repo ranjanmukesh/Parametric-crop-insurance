ca7c3e5a2b3e2cc3798da789e467ba7db2707cf4
Mukesh Ranjan <mukesh.ranjan@tutamail.com>
2026-03-31 05:34:52 +0000

    fixed compilation issue

--- test/ParametricCropInsurance.t.sol
+++ test/ParametricCropInsurance.t.sol
@@ -71,7 +71,7 @@ contract ParametricCropInsuranceTest is Test {
   uint256 constant MED_DROUGHT_THRESHOLD = 750;
   uint256 constant MED_EXCESS_THRESHOLD = 1400;
   uint256 constant HIGH_DROUGHT_THRESHOLD = 850;
-  uint256 constant HIGH_EXCESS_THRESHOLD;
+  uint256 constant HIGH_EXCESS_THRESHOLD = 1250;
   uint256 constant EXPECTED_RAINFALL = 120;
   string constant SEASON_START = "2025-06-01";
   string constant SEASON_END = "2025-09-30";
